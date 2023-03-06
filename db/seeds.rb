715546dc637745cda997d0bd8338d46168581619
Edouard de Montchenu edouarddemontchenu@gmail.com <edouarddemontchenu@gmail.com>
2023-03-06 17:46:46 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "csv"
+
+filepath = "storage/items.csv"
+
+CSV.foreach(filepath, headers: :first_row) do |row|
+  Item.new(name: row[0], desc: row[1], way: row[2], price: 0)
+end
