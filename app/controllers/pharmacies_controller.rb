3798bd5b87cda2369ece28b39920cee559336811
Edouard de Montchenu edouarddemontchenu@gmail.com <edouarddemontchenu@gmail.com>
2023-03-08 16:17:09 +0100

    map

--- app/controllers/pharmacies_controller.rb
+++ app/controllers/pharmacies_controller.rb
@@ -1,7 +1,23 @@
 class PharmaciesController < ApplicationController
+  before_action :set_prescription
   def index
+    @pharmacies = Pharmacy.all
+    @markers = @pharmacies.geocoded.map do |pharmacy|
+      {
+        lat: pharmacy.latitude,
+        lng: pharmacy.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: { pharmacy: pharmacy})
+      }
+    end
   end
 
   def show
+    @pharmacy = Pharmacy.find(params[:id])
+  end
+
+  private
+
+  def set_prescription
+    @prescription = Prescription.find(params[:prescription_id])
   end
 end
