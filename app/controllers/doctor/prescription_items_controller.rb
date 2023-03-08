3e996cb1232de57da43a3a04cac3424e1df8d2ee
Fabien James <fabts4@gmail.com>
2023-03-08 18:26:17 +0100

    V1

--- app/controllers/doctor/prescription_items_controller.rb
+++ app/controllers/doctor/prescription_items_controller.rb
@@ -1,5 +1,13 @@
 class Doctor::PrescriptionItemsController < ApplicationController
   def create
+    @prescription = Prescription.find(params[:prescription_id])
+    @prescription_item = PrescriptionItem.new(item_params)
+    @prescription_item.prescription = @prescription
+    if @prescription_item.save
+      redirect_to edit_doctor_prescription_path(@prescription)
+    else
+      render 'edit_doctor_prescription', status: :unprocessable_entity
+    end
   end
 
   def update
@@ -7,4 +15,11 @@ class Doctor::PrescriptionItemsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def item_params
+    params.require(:prescription_item).permit(:item_id, :posology)
+  end
+
 end
--- app/controllers/doctor/prescriptions_controller.rb
+++ app/controllers/doctor/prescriptions_controller.rb
@@ -1,13 +1,34 @@
 class Doctor::PrescriptionsController < ApplicationController
   def index
+    @prescription = Prescription.new
   end
 
   def show
   end
 
   def edit
+    @prescription = Prescription.find(params[:id])
+    @prescription_items = PrescriptionItem.where(prescription_id: @prescription.id)
+    @prescription_item = PrescriptionItem.new
+    @items = Item.where(drug: true)
   end
 
   def update
+  end
+
+  def create
+    if params[:patient_id].nil?
+      @prescription = Prescription.new
+      render 'doctor/prescriptions/index', status: :unprocessable_entity
+    else
+      @prescription = Prescription.find_or_create_by(prescription_params.merge(doctor: current_user))
+      redirect_to edit_doctor_prescription_path(@prescription)
+    end
+  end
+
+  private
+
+  def prescription_params
+    params.require(:prescription).permit(:patient_id)
   end
 end
