94e06a14f7c55563e219895e70b4f7058015a677
Edouard de Montchenu edouarddemontchenu@gmail.com <edouarddemontchenu@gmail.com>
2023-03-09 13:13:18 +0100

    geoloc

--- app/controllers/pharmacies_controller.rb
+++ app/controllers/pharmacies_controller.rb
@@ -7,17 +7,10 @@ class PharmaciesController < ApplicationController
       {
         lat: pharmacy.latitude,
         lng: pharmacy.longitude,
-        info_window_html: render_to_string(partial: "info_window", locals: { pharmacy: pharmacy})
+        info_window_html: render_to_string(partial: "info_window", locals: { pharmacy: pharmacy}),
+        marker_html: render_to_string(partial: "marker")
       }
     end
-    # @users = User.all
-    # @markers = @users.geocoded.map do |user|
-    #   {
-    #     lat: current_user.latitude,
-    #     lng: current_user.longitude,
-    #     info_window_html: render_to_string(partial: "info_window", locals: { user: current_user })
-    #   }
-    # end
   end
 
   def show
--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -8,20 +8,24 @@ export default class extends Controller {
   }
 
   connect() {
-   console.log('Hello')
    mapboxgl.accessToken = this.apiKeyValue
    this.map = new mapboxgl.Map({
      container: this.element,
      style: "mapbox://styles/mapbox/streets-v10"
    })
-   this.#addMarkersToMap()
-   this.#fitMapToMarkers()
+
+   this.#addMarkersToMap();
+   this.#fitMapToMarkers();
+   this.#currentPosition();
   }
 
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
       const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
-      new mapboxgl.Marker()
+      const customMarker = document.createElement("div")
+      customMarker.innerHTML = marker.marker_html
+
+      new mapboxgl.Marker(customMarker)
         .setLngLat([ marker.lng, marker.lat ])
         .setPopup(popup)
         .addTo(this.map)
@@ -32,5 +36,17 @@ export default class extends Controller {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
-    }
+  }
+
+  #currentPosition() {
+    navigator.geolocation.getCurrentPosition(((success) => {
+      const lng = success.coords.longitude
+      const lat = success.coords.latitude
+
+      new mapboxgl.Marker({
+        color: "#E60F05"
+      })
+      .setLngLat([ lng, lat ]).addTo(this.map)
+    }));
+  }
 }
