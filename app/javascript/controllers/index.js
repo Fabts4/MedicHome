7db90c5e20c82db2ea5e943d2da2bac49c3c4a88
Fabien James <fabts4@gmail.com>
2023-03-16 16:02:30 +0100

    done

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -20,7 +20,7 @@ import TomSelectController from "./tom_select_controller"
 application.register("tom-select", TomSelectController)
 
 import TomSelect2Controller from "./tom_select_2_controller"
-application.register("tom-select-2", TomSelect2Controller)
+application.register("tom-select", TomSelect2Controller)
 
 import TotalController from "./total_controller"
 application.register("total", TotalController)
